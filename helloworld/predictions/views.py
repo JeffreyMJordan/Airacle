9c323adbb227a4385e5c936f8f4a35de2873ca22
JeffreyMJordan <jeff2016@alumni.stanford.edu>
2017-12-11 18:39:20 -0800

    Sets up home request for predictions

--- helloworld/predictions/views.py
+++ helloworld/predictions/views.py
@@ -1,8 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse 
 from estimators.models import Estimator
+from django.views.decorators.csrf import csrf_exempt
+import json
 
+
+@csrf_exempt
 def home(request):
-  ests = Estimator.objects.all()
-  print(ests)
-  return HttpResponse("Ayyy lamo")
+  
+  if request.method == "POST":
+    thing = request.POST.get('key', 'N/A')
+    json_object = json.loads(thing)
+    return HttpResponse("POST")
+  else:
+    return HttpResponse("GET")
