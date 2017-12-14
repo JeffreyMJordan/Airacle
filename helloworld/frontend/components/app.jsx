43b63937e410a3af20b889197bc6ac6ce6c52352
JeffreyMJordan <jeff2016@alumni.stanford.edu>
2017-12-13 18:22:50 -0800

    Fixes form bug

--- helloworld/frontend/components/app.jsx
+++ helloworld/frontend/components/app.jsx
@@ -1,14 +1,12 @@
 import React from 'react';
 import FormContainer from './form/form_container';
-<<<<<<< HEAD
-=======
-import GraphContainer from './graph/graph_container';
->>>>>>> dac9934a63de94cdea8a52bcc02e8b7b6b52dd58
 import Graph from './graph/graph';
+import GraphContainer from './graph/graph_container';
 
 const App = () => (
   <div>
-    <GraphContainer />
+    <FormContainer />
+    {/* <GraphContainer /> */}
   </div>
 );
 
