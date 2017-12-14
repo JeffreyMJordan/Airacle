c037d1a2b7cd6e5ed5fe4f1ac5717362c58aedff
JeffreyMJordan <jeff2016@alumni.stanford.edu>
2017-12-13 18:33:55 -0800

    Wraps App in HashRouter and adds routes for Form and Graph

--- helloworld/frontend/components/app.jsx
+++ helloworld/frontend/components/app.jsx
@@ -2,11 +2,12 @@ import React from 'react';
 import FormContainer from './form/form_container';
 import Graph from './graph/graph';
 import GraphContainer from './graph/graph_container';
+import {Route} from 'react-router-dom';
 
 const App = () => (
   <div>
-    <FormContainer />
-    {/* <GraphContainer /> */}
+    <Route exact path="/" component={FormContainer} />
+    <Route path="/graph" component={GraphContainer} />
   </div>
 );
 
