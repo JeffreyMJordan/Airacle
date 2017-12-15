24b1e5232a29ec9450ec3cbb3a40985aead9d5c6
JeffreyMJordan <jeff2016@alumni.stanford.edu>
2017-12-15 09:43:26 -0800

    Adds dropdown.css styling for dropdown form

--- helloworld/frontend/components/form/form.jsx
+++ helloworld/frontend/components/form/form.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import {withRouter} from "react-router-dom";
 import Select from 'react-select';
+// import 'react-select/dist/react-select.css';
 
 class Form extends React.Component {
   constructor(props) {
